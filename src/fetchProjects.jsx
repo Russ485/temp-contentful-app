119c217bb0d5cc8a8fc38c1f320d81b89f390358
Russ485 <kazyonnylawyer@gmail.com>
2025-10-26 18:14:20 +0100

    first commit

--- src/fetchProjects.jsx
+++ src/fetchProjects.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 const client = createClient({
   space: "eri3n4i64db6",
   environment: "master",
-  accessToken: "66R8V663H2YELcnq0_ohA2fq71fvvy7inekonRNw1vU",
+  accessToken: import.meta.env.VITE_API_KEY,
 });
 
 export const useFetchProjects = () => {
